c6548e915d38ebd05f6fd731704e9b984813192d
Mithil <mithil467@gmail.com>
2019-10-11 23:04:30 +0530

    Initial Commit

---
+++ values.js
@@ -0,0 +1,17 @@
+var PipeColor = "yellow";
+var PlayerColor = "pink";
+
+var BirdSpeed = 1;
+var PipeSpeed = 1;
+var PipeGap = 50;
+
+var UpKey = "ArrowLeft";
+var DownKey = "ArrowRight";
+
+var Message = "DEAD!";
+var GameName = "Help Me DODGE viper!";
+var DisplayScore = true;
+
+/*
+https://www.w3schools.com/colors/colors_names.asp
+*/
